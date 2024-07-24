492a3a8b0c0a2a63225cc3334e5d3358e8b7a143
Seogo <77395226+ksh6940@users.noreply.github.com>
2024-07-24 15:44:31 +0900

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,13 +1,16 @@
 #include <L298N.h>
 
-const int buzzerPin = 13;
+const int buzzerPin = 7;
 
-const unsigned int IN1 = 8;
-const unsigned int IN2 = 9;
-const unsigned int EN = 10;
+const unsigned int EN = 8;
+const unsigned int IN1 = 9;
+const unsigned int IN2 = 10;
 
 const int buttonPin1 = 2;
-const int buttonPin2 = 4; 
+const int buttonPin2 = 3; 
+
+const int LedPin1 = 12;
+const int LedPin2 = 13;
 
 L298N motor(EN, IN1, IN2);
 
@@ -18,6 +21,8 @@ void setup()
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
 
+  pinMode(LedPin1, OUTPUT);
+  pinMode(LedPin2, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 
   motor.setSpeed(100);
@@ -30,17 +35,23 @@ void loop()
 
   tone(buzzerPin, 329.63);
 
- if (buttonState1 == HIGH){
-   Serial.println('버튼 : 가까워짐');
-   motor.forward();
-   delay(6000);
-   motor.stop();
- } else if (buttonState2 == HIGH) {
-   Serial.println('버튼 : 멀어짐');
-   motor.backward();
-   delay(6000);
-   motor.stop();
- } else {
-   motor.stop();
- }
+  if (buttonState1 == LOW) {
+    Serial.println("버튼 : 가까워짐");  // Corrected to use double quotes for string
+    digitalWrite(LedPin2, HIGH);
+    motor.forward();
+    delay(6000);
+    motor.stop();
+  } 
+  else if (buttonState2 == LOW) {
+    Serial.println("버튼 : 멀어짐");  // Corrected to use double quotes for string
+    digitalWrite(LedPin1, HIGH);
+    motor.backward();
+    delay(6000);
+    motor.stop();
+  } 
+  else {
+    digitalWrite(LedPin1, LOW);  // Turn off both LEDs when neither button is pressed
+    digitalWrite(LedPin2, LOW);
+    motor.stop();
+  }
 }
